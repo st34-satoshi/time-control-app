8b3cba2ad4258ceda1abfbecd8a17e14d95ab9ea
Satoshi Tanaka <st34.satoshi@gmail.com>
2025-06-29 12:23:48 +0900

    時間記録画面

--- App.tsx
+++ App.tsx
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { styles } from './App.styles';
 import { Header } from '@components/Header';
 import { Footer } from '@components/Footer';
+import TimeRecordScreen from '@components/TimeRecordScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-      
-      <View style={styles.content}>
-        <Text>Hello World</Text>
-      </View>
-      
+      <TimeRecordScreen />
       <Footer />
-      
-      <StatusBar style="auto" />
     </View>
   );
 }
